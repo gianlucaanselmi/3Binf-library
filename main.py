7e66c4d5ae1440dcf8053a488d470b4fcbd9c45e
Your Name <you@example.com>
2019-10-12 09:54:26 +0200

    addinit

---
+++ main.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self,title,author,sbn):
+        self.title = "no title"
+        self.author ="no author"
+        self.sbn = 0
+    
+harry_potter = Book("Harry potter e il calice di fuoco","J.K:Rowling",1)
+print(harry_potter.title)
